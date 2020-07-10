aaab1f37a7f8b8cb328b8a6acf08fb832873761c
Héctor Hurtado <hector.hurtado@bbva.com>
2020-07-10 16:20:11 +0200

    Change rule's code to 'show' evicences

--- fortitude/impl/rule/main.sh
+++ fortitude/impl/rule/main.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 cat <<-EOD
-  Hello World!
-  From $source to $target with $level
+  Checking $target security group's default policy = deny: correct!
+  Checking $target security group's rule count = 1: correct!
+  Checking rule source = $source: correct!
+  Checking rule level = $level: correct!
 EOD
+
+# Hello World!
+# From $source to $target with $level
